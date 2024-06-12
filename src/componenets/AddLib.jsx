00444dbe635fb6363d68060742d75b24e727eb96
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-12 21:30:22 +0530

    routing

--- src/componenets/AddLib.jsx
+++ src/componenets/AddLib.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavLib from './NavLib'
 
 const AddLib = () => {
   return (
     <div>
+        <NavLib/>
         <div className="container">
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
