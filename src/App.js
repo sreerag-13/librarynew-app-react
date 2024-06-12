36ee5e85ae7ed8e85c698ad960c79305a41a77a2
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-12 20:56:48 +0530

    sample search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddLib from './componenets/AddLib';
+import SearchLib from './componenets/SearchLib';
 
 function App() {
   return (
     <div>
       <AddLib/>
+      <SearchLib/>
     
     </div>
   );
