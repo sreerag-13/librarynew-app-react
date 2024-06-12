65fa420ad540be9d2606fa1bcadb2420576989d3
sreerag-13 <sreeragsaji41@gmail.com>
2024-06-12 21:07:44 +0530

    view sample

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddLib from './componenets/AddLib';
 import SearchLib from './componenets/SearchLib';
+import ViewLib from './componenets/ViewLib';
 
 function App() {
   return (
     <div>
       <AddLib/>
       <SearchLib/>
+      <ViewLib/>
     
     </div>
   );
